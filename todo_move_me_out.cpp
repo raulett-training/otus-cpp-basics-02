a618620d360d1fc2fa32808e09969ccc6582745a
raulett-homePC\raulett <raulett@gmail.com>
2023-04-09 16:42:30 +0800

    Refactored todo_move_me_out

---
+++ todo_move_me_out.cpp
@@ -0,0 +1,10 @@
+//
+// Created by raulett on 09.04.2023.
+//
+#include <iostream>
+#include "todo_move_me_out.h"
+
+
+void todo_move_me_out() {
+    std::cout << "This function should be located in the separated source file" << std::endl;
+}
